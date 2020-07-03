0a23e6b8a872c6768aab7a341d5ea5cd8e8f3e4d
Bouris Dimitrios <std133533@ac.eap.gr>
2020-07-03 21:11:52 +0300

    Update leaflet-gsheets.js

--- leaflet-gsheets.js
+++ leaflet-gsheets.js
@@ -209,21 +209,4 @@ function getColor(type) {
     return "green";
   }
   
- var RADIUS = 500000;
-  
-  var filterCircle = L.circle(L.latLng(40, -75), RADIUS, {
-    opacity: 1,
-    weight: 1,
-    fillOpacity: 0.4
-}).addTo(map);
-
-  
-  map.on('mousemove', function(e) {
-    filterCircle.setLatLng(e.latlng);
-    csvLayer.setFilter(function showAirport(feature) {
-        return e.latlng.distanceTo(L.latLng(
-                feature.geometry.coordinates[1],
-                feature.geometry.coordinates[0])) < RADIUS;
-    });
-});
 }
